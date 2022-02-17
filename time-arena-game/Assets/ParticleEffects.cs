e98e40b1e82d5f8d14deff75f3edd18c3b95ff20
PaoloMura <paolo.mura@streamsets.com>
2022-02-17 11:43:58 +0000

    colour changes depending on direction of time travel

--- time-arena-game/Assets/ParticleEffects.cs
+++ time-arena-game/Assets/ParticleEffects.cs
@@ -8,24 +8,55 @@ public class PlayerParticleEffects : MonoBehaviour
     public ParticleSystem fireCircle;
     public ParticleSystem splash;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Color ORANGE = new Color(1.0f, 0.46f, 0.19f, 1.0f);
+    Color BLUE = new Color(0.19f, 0.38f, 1.0f, 1.0f);
+    Color WHITE = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
-    // Update is called once per frame
     // TODO: this is for testing purposes only - remove later
     void Update()
     {
         if ( Input.GetKeyDown( KeyCode.J ) )
         {
-            TimeJump();
+            JumpForward();
+        }
+        else if ( Input.GetKeyDown( KeyCode.K ) )
+        {
+            JumpBackward();
         }
     }
 
-    void TimeJump()
+    void JumpForward()
     {
+        var fcm = fireCircle.main;
+        fcm.startColor = BLUE;
+
+        var fct = fireCircle.trails;
+        fct.colorOverTrail = BLUE;
+
+        var sm = splash.main;
+        sm.startColor = WHITE;
+
+        var st = splash.trails;
+        st.colorOverTrail = BLUE;
+
+        fireCircle.Play();
+        splash.Play();
+    }
+
+    void JumpBackward()
+    {
+        var fcm = fireCircle.main;
+        fcm.startColor = ORANGE;
+
+        var fct = fireCircle.trails;
+        fct.colorOverTrail = ORANGE;
+
+        var sm = splash.main;
+        sm.startColor = WHITE;
+
+        var st = splash.trails;
+        st.colorOverTrail = ORANGE;
+
         fireCircle.Play();
         splash.Play();
     }
