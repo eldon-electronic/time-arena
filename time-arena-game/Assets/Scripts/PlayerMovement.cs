29621934e150489d650d8c11b5d02ea7417d3345
PaoloMura <paolo@muraonline.com>
2022-02-21 12:37:37 +0000

    increased gravity

--- time-arena-game/Assets/Scripts/PlayerMovement.cs
+++ time-arena-game/Assets/Scripts/PlayerMovement.cs
@@ -24,8 +24,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	public int team = 1;//0 seeker 1 hider //iniitialised to 0 but changeTeam is called on start to sync values
 	private float speed = 5f;
-	private float gravity = 10f;
-	private float jumpPower = 10f;
+	private float gravity = 40f;
+	private float jumpPower = 3f;
 	private float groundCheckRadius = 0.5f;
 	private bool isGrounded = true;
 	private Vector3 velocity;
