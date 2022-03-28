d13a40b50745f62cfa639fcca48e94db3a03cd81
PaoloMura <paolo@muraonline.com>
2022-03-28 11:38:05 +0100

    added comments

--- time-arena-game/Assets/Scripts/FrameData.cs
+++ time-arena-game/Assets/Scripts/FrameData.cs
@@ -18,14 +18,20 @@ public class FrameData
         _lastTailID = 0;
     }
 
+    // Returns this player's perceived frame.
     public int GetPerceivedFrame() { return _perceivedFrame; }
 
+    // Adds the given offset to the perceived frame.
     public void OffsetPerceivedFrame(int offset) { _perceivedFrame += offset; }
 
+    // Returns this player's tail frames (the pointers to where we're writing to in the player states structure).
+    // There may be 0, 1 or 2 tail frames at any one time.
     public List<int> GetTailFrames() { return _tailFrames; }
 
+    // Returns the tail frame pointer of the latest reality we're writing to.
     public int GetLatestFrame() { return _tailFrames.Last(); }
 
+    // Increments every frame (perceived frame and tail frames).
     public void Increment()
     {
         _perceivedFrame++;
@@ -35,20 +41,24 @@ public class FrameData
         }
     }
 
+    // Returns the tail ID of the last reality we were writing to.
     public int GetLastTailID() { return _lastTailID; }
 
+    // Returns the tail ID of the next reality we are writing to.
     public int GetNextTailID()
     {
         if (_tailFrames.Count <= 1) return _lastTailID;
         else return _lastTailID + 1;
     }
 
+    // Start writing to a new reality at the given frame. i.e. add a tail writer pointer.
     public void AddTail(int frame)
     {
         if (_tailFrames.Count == 2) Debug.LogError("Cannot write to more than two tails simultaneously.");
         else _tailFrames.Add(frame);
     }
 
+    // Stop writing to the last reality you were at. i.e. remove the first tail writer pointer.
     public void RemoveTail()
     {
         if (_tailFrames.Count == 0) Debug.LogError("No tracked tails to remove.");
--- time-arena-game/Assets/Scripts/RealityManager.cs
+++ time-arena-game/Assets/Scripts/RealityManager.cs
@@ -13,13 +13,13 @@ public class RealityManager
         _heads = new Dictionary<int, FrameData>();
     }
 
-    public int Size() { return _heads.Count; }
-
+    // Add a new player to the dictionary.
     public void AddHead(int playerID)
     {
         _heads.Add(playerID, new FrameData());
     }
 
+    // Increment the frame values of every player.
     public void Increment()
     {
         foreach (var head in _heads)
@@ -28,12 +28,14 @@ public class RealityManager
         }
     }
 
+    // Return the perceived frame of the given player.
     public int GetPerceivedFrame(int playerID)
     {
         FrameData frames = _heads[playerID];
         return frames.GetPerceivedFrame();
     }
 
+    // Return the player ID and perceived frame of all players in the dictionary.
     public List<(int id, int frame)> GetPerceivedFrames()
     {
         List<(int, int)> frames = new List<(int, int)>();
@@ -44,28 +46,37 @@ public class RealityManager
         return frames;
     }
 
+    // Add the given offset to the given player's perceived frame.
     public void OffsetPerceivedFrame(int playerID, int offset)
     {
         FrameData frames = _heads[playerID];
         frames.OffsetPerceivedFrame(offset);
     }
 
+    // Return the tail writer pointers of the given player.
+    // Each player may have 0, 1 or 2 tail frames at any time.
     public List<int> GetTailFrames(int playerID)
     {
         FrameData frames = _heads[playerID];
         return frames.GetTailFrames();
     }
 
+    // Make the given player start writing their state to a new reality,
+    // starting from the given frame.
+    // i.e. add a new tail writer pointer with the given frame.
     public void AddTail(int playerID, int frame)
     {
         _heads[playerID].AddTail(frame);
     }
 
+    // Remove the earliest tail writer pointer from the given player.
     public void RemoveTail(int playerID)
     {
         _heads[playerID].RemoveTail();
     }
 
+    // Given a player and their frame, return the closest frame of a different player.
+    // This should be from the last reality they were writing to.
     public int GetClosestFrame(int playerID, int frame)
     {
         int closest = int.MaxValue;
@@ -83,6 +94,9 @@ public class RealityManager
         return closest;
     }
 
+    // TODO: Use this function for finding out which heads (actual players) you need to render.
+    // Return a list of player IDs for those players who exist in the same reality
+    // as the given frame.
     public List<int> GetHeadsInFrame(int frame)
     {
         List<int> heads = new List<int>();
@@ -96,7 +110,9 @@ public class RealityManager
         return heads;
     }
 
+    // Return the tail ID of the last reality the given player was writing to.
     public int GetLastTailID(int playerID) { return _heads[playerID].GetLastTailID(); }
 
+    // Return the tail ID of the current reality the given player is writing to.
     public int GetNextTailID(int playerID) { return _heads[playerID].GetNextTailID(); }
 }
