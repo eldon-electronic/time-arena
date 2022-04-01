c81ec52954ad5992ac8cfe05539e45caf12211a6
Angus Robertson <angusprobertson@gmail.com>
2022-04-01 13:39:10 +0100

    time crystal animation started

--- time-arena-game/Assets/Scripts/CrystalBehaviour.cs
+++ time-arena-game/Assets/Scripts/CrystalBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CrystalBehaviour : MonoBehaviour
 {
@@ -8,10 +9,14 @@ public class CrystalBehaviour : MonoBehaviour
     public float initial_wave = 0;
     public float offsetY = 0;
     public float offsetRot = 0;
+    private float t = 0;
     // Update is called once per frame
     void Update()
     {
-      overlay.SetFloat("Wave_Incr", initial_wave+0.001f);
-
+      t += Time.deltaTime;
+      offsetY = (float)(0.5 * Math.Sin(t))-1;
+      gameObject.transform.position = (new Vector3(0.0f, offsetY, 0.0f));
+      transform.Rotate(0.0f, 10f*Time.deltaTime, 0.0f, Space.Self);
+      overlay.SetFloat("Wave_Incr", 10f*t);
     }
 }
