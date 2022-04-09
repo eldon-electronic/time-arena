308546c2067e9893cd719cd913092d74e33a71f7
PaoloMura <paolo@muraonline.com>
2022-04-09 17:48:30 +0100

    refactored HUD tutorial into its own script

--- time-arena-game/Assets/Scripts/Player/PlayerHud.cs
+++ time-arena-game/Assets/Scripts/Player/PlayerHud.cs
@@ -10,6 +10,7 @@ using TMPro;
 public class PlayerHud : MonoBehaviour
 {
     private GameController _game;
+    [SerializeField] private PhotonView _view;
     [SerializeField] HudStartTimer _startTimer;
     [SerializeField] HudTimeDisplay _timeDisplay;
     [SerializeField] HudMasterClientOptions _masterClientOptions;
@@ -18,45 +19,15 @@ public class PlayerHud : MonoBehaviour
     [SerializeField] HudWinningDisplay _winningDisplay;
     [SerializeField] HudScore _score;
     [SerializeField] HudDebugPanel _debugPanel;
-	public PhotonView View;
+    [SerializeField] HudTutorial _tutorial;
     public Text TeamDispl;
-    public GameObject ArrowImage;
-    public GameObject Tutorial;
-    public GameObject PopUpText;
-    public GameObject OptionsPopUpText;
 
     private float _secondsTillGame;
 	private bool _isCountingTillGameStart;
-    private string _message;
-    private string _optionsMessage;
-
-
-    private Dictionary<string, Vector2> _uiPositions;
-    private Dictionary<string, Vector3> _uiRotations;
-    
-
 
     void Start()
     {
-
-
-        _uiPositions = new Dictionary<string, Vector2>();
-        _uiPositions.Add("forwardJump",new Vector2(381f,-76f));
-        _uiPositions.Add("backJump",new Vector2(-397f,-82f));
-        _uiPositions.Add("timebar",new Vector2(-342f,-122f));
-        _uiPositions.Add("timer",new Vector2(-290f,105f));
-        _uiPositions.Add("team",new Vector2(284f,104f));
-
-
-        _uiRotations = new Dictionary<string, Vector3>();
-        _uiRotations.Add("forwardJump",new Vector3(-10.1f,-720.2f,18.5f));
-        _uiRotations.Add("backJump",new Vector3(-10.1f,-535.5f,4.7f));
-        _uiRotations.Add("timebar",new Vector3(-10.1f,-535.5f,4.7f));
-        _uiRotations.Add("timer",new Vector3(-10.1f,-535.5f,-150f));
-        _uiRotations.Add("team",new Vector3(-10.1f,-355.5f,-150f));
-
-
-        if (View.IsMine)
+        if (_view.IsMine)
         {
             if (!PhotonNetwork.IsMasterClient)
             {
@@ -76,24 +47,13 @@ public class PlayerHud : MonoBehaviour
     }
 
 
-    // ------------ LATE UPDATE HELPER FUNCTIONS ------------
-
-    private void LateUpdateTutorial()
-    {
-        if (SceneManager.GetActiveScene().name != "PreGameScene")
-        {
-            Tutorial.gameObject.SetActive(false);
-        }
-    }
-
-
     // ------------ UPDATE METHODS ------------
 
     void Update()
     {
         // TODO: Get this out of here!!! This kind of power belongs in GameController or PlayerController at the very least!
         // If counting, reduce timer.
-        if (PhotonNetwork.IsMasterClient && _isCountingTillGameStart && View.IsMine) {
+        if (PhotonNetwork.IsMasterClient && _isCountingTillGameStart && _view.IsMine) {
             _secondsTillGame -= Time.deltaTime;
             _masterClientOptions.SetSecondsTillGame(_secondsTillGame);
             if (_secondsTillGame <= 0) {
@@ -105,14 +65,6 @@ public class PlayerHud : MonoBehaviour
     }
 
 
-    void LateUpdate()
-    {
-        if (!View.IsMine) return;
-        LateUpdateTutorial();
-
-    }
-
-
     // ------------ PUBLIC METHODS ------------
 
     public void setScore(int score) { _score.SetScore(score); }
@@ -121,9 +73,8 @@ public class PlayerHud : MonoBehaviour
 
     public void SetTeam(System.String teamName)
     {
-        if (View.IsMine) TeamDispl.text = teamName;
+        if (_view.IsMine) TeamDispl.text = teamName;
     }
-
 
     public void StartCountingDown()
     {
@@ -135,7 +86,6 @@ public class PlayerHud : MonoBehaviour
         _masterClientOptions.SetSecondsTillGame(_secondsTillGame);
     }
 
-
     public void StopCountingDown()
     {
         _isCountingTillGameStart = false;
@@ -143,7 +93,6 @@ public class PlayerHud : MonoBehaviour
         _secondsTillGame = 0.0f;
         _masterClientOptions.SetSecondsTillGame(_secondsTillGame);
     }
-
 
     public void SetDebugValues(Hashtable items)
     {
@@ -171,13 +120,13 @@ public class PlayerHud : MonoBehaviour
 
     public void PressForwardJumpButton()
     {
-        if (!View.IsMine) return;
+        if (!_view.IsMine) return;
         _cooldowns.PressForwardJumpButton();
     }
 
     public void PressBackJumpButton()
     {
-        if (!View.IsMine) return;
+        if (!_view.IsMine) return;
         _cooldowns.PressBackJumpButton();
     }
 
@@ -186,43 +135,34 @@ public class PlayerHud : MonoBehaviour
         _cooldowns.SetCanJump(forward, back);
     }
 
-    public void SetArrowPosition(string uiElement){
-
-        if (!View.IsMine) return;
-
-        ArrowImage.GetComponent<RectTransform>().anchoredPosition = _uiPositions[uiElement];
-        ArrowImage.GetComponent<RectTransform>().eulerAngles = _uiRotations[uiElement];
+    public void SetArrowPosition(string uiElement)
+    {
+        if (!_view.IsMine) return;
+        _tutorial.SetArrowPosition(uiElement);
     }
 
-    public void SetMessage(string tutorialMessage){
-
-        if (!View.IsMine) return;
-
-        _message = tutorialMessage;
-        PopUpText.GetComponent<TextMeshProUGUI>().text = _message;
-
+    public void SetMessage(string tutorialMessage)
+    {
+        if (!_view.IsMine) return;
+        _tutorial.SetMessage(tutorialMessage);
     }
 
-    public void SetArrowVisibility(bool arrowVisibility){
-
-        if (!View.IsMine) return;
-
-        ArrowImage.gameObject.SetActive(arrowVisibility);
+    public void SetArrowVisibility(bool arrowVisibility)
+    {
+        if (!_view.IsMine) return;
+        _tutorial.SetArrowVisibility(arrowVisibility);
     }
-    public void SetTutorialVisibility(bool tutorialVisibility){
 
-        if (!View.IsMine) return;
-        
-        Tutorial.gameObject.SetActive(tutorialVisibility);
-
+    public void SetTutorialVisibility(bool tutorialVisibility)
+    {
+        if (!_view.IsMine) return;
+        _tutorial.SetVisibility(tutorialVisibility);
     }
-    public void SetOptionsText(string optionsMessage){
 
-        if (!View.IsMine) return;
-
-        _optionsMessage = optionsMessage;
-        OptionsPopUpText.GetComponent<TextMeshProUGUI>().text = _optionsMessage;
-
+    public void SetOptionsText(string optionsMessage)
+    {
+        if (!_view.IsMine) return;
+        _tutorial.SetOptionsText(optionsMessage);
     }
     
     public void SetGame(GameController game)
