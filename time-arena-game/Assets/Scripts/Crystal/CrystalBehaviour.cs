a365c751ad2ffcbb9bb77b61e48830c90e9c2ac1
Angus Robertson <angusprobertson@gmail.com>
2022-04-14 00:38:17 +0100

    made it pwetty

--- time-arena-game/Assets/Scripts/Crystal/CrystalBehaviour.cs
+++ time-arena-game/Assets/Scripts/Crystal/CrystalBehaviour.cs
@@ -6,16 +6,22 @@ using static System.Math;
 
 public class CrystalBehaviour : MonoBehaviour
 {
+    //attributes relating to anim/aesthetics
     public Material overlay;
     public float initial_wave = 0;
     private float t = 0;
 
+    //attributes for crystalmanager access
     private CrystalManager cm;
     public int ID;
+    private GameController game;
 
+    //attributes defining crystal state
     public Vector2 existanceRange = new Vector2(5f, 10f);
-    public bool isCollected = false;
+    public bool isCollected = false; //if isCollected is true - there is no instance of the crystal at any time
 
+    //called upon player collision
+    // - crystal will be set to inactive in following frame so coroutine outsourced to cm
     public void Collect(){
       existanceRange = new Vector2(-1f, -1f);
       isCollected = true;
@@ -24,9 +30,14 @@ public class CrystalBehaviour : MonoBehaviour
       }
     }
 
+    // Start is called before the first frame
     void Start(){
       initial_wave = Random.Range(5f, 10f);
+      game = GameObject.FindGameObjectsWithTag("TimeLord")[0].GetComponent<GameController>();
       cm = GameObject.FindGameObjectsWithTag("TimeLord")[0].GetComponent<CrystalManager>();
+
+      //on instantiation, add self to crystal list in cm
+      //give self id according to position in list (syncing doesnt matter, only uniqueness and size)
       ID = cm.crystals.Count;
       cm.crystals.Add(this);
     }
@@ -37,14 +48,32 @@ public class CrystalBehaviour : MonoBehaviour
       UpdateAnim();
     }
 
+    //update aesthetics
     void UpdateAnim(){
       t += Time.deltaTime;
       float offsetY = (float)(0.01 * Sin(t));
       gameObject.transform.Translate(new Vector3(0.0f, offsetY, 0.0f));
       transform.Rotate(0.0f, 30f*Time.deltaTime, 0.0f, Space.Self);
       overlay.SetFloat("Wave_Incr", t);
+
+      //zoom into and out of existance
+      float percievedTime = (game._timeLord.GetYourPosition()*game._timeLord._totalFrames) / Constants.FrameRate;
+      float closestBorderOFExistance = Min(Abs(percievedTime - existanceRange[0]), Abs(percievedTime - existanceRange[1]));
+      float animLength = 2.0f;
+      if(closestBorderOFExistance > animLength){
+        setScale(1.0f);
+      } else {
+        setScale(closestBorderOFExistance/animLength);
+      }
     }
 
+    //setScale
+    void setScale(float a){
+      transform.localScale = new Vector3(a, a, a);
+    }
+
+    //set existance range (period of time when crystal exists in game)
+    //i.e. spawn a new crystal (called by coroutine after x seconds)
     [PunRPC]
   	void RPC_setExistanceRange(Vector2 newRange)
   	{
--- time-arena-game/Assets/Scripts/Crystal/CrystalManager.cs
+++ time-arena-game/Assets/Scripts/Crystal/CrystalManager.cs
@@ -6,22 +6,28 @@ using Photon.Pun;
 public class CrystalManager : MonoBehaviour
 {
 
+  //references to gameobjects
   [SerializeField] private GameObject spawnHolder;
   [SerializeField] private GameObject crystalPrefab;
   [SerializeField] private GameController game;
 
+  //spawnPoints will have a location of every single crystal
+  //order is shared with crystals list so can be iterated over simultaneously
+  //spawnpoints necessary for crystal instantiation - add more by adding spawn points in editor
   private Transform[] spawnPoints;
   public List<CrystalBehaviour> crystals = new List<CrystalBehaviour>();
 
     // Start is called before the first frame update
     void Start()
     {
+      //find all spawnpoints in gamescene
       spawnPoints = new Transform[spawnHolder.transform.childCount];
       int i = 0;
       foreach(Transform child in spawnHolder.transform){
         spawnPoints[i++] = child;
       }
 
+      //instantiate crystals (they will populate the crystals list themselves)
       foreach(Transform spawnPoint in spawnPoints){
         PhotonNetwork.Instantiate(crystalPrefab.name, spawnPoint.position, Quaternion.identity);
       }
@@ -30,6 +36,8 @@ public class CrystalManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      //check if player should be able to see crystals
+      //TODO::> set crystal layers???
       foreach(CrystalBehaviour crystal in crystals){
         float percievedTime = (game._timeLord.GetYourPosition()*game._timeLord._totalFrames) / Constants.FrameRate;
         if(percievedTime >= crystal.existanceRange[0] && percievedTime <= crystal.existanceRange[1]){
@@ -40,12 +48,14 @@ public class CrystalManager : MonoBehaviour
       }
     }
 
-
+    //enumerator coroutine to be called when crystal is collected - waits x seconds before respawning crystal at random time
     public IEnumerator Respawn(int id){
+      float spawnDelay = 5.0f;
       float newExistance = Random.Range(0.0f, (game._timeLord.GetTimeProportion()*game._timeLord._totalFrames)/Constants.FrameRate);
       Vector2 newExistanceRange = new Vector2(newExistance, newExistance + 10f );
-      yield return new WaitForSeconds(1.0f);
+
+      yield return new WaitForSeconds(spawnDelay);
+
       crystals[id].gameObject.GetComponent<PhotonView>().RPC("RPC_setExistanceRange", RpcTarget.All, newExistanceRange);
-      Debug.Log("check2");
     }
 }
