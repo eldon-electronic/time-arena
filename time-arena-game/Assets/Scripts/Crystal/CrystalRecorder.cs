507dbfa07f094e619c2385015ab466183b3aba27
Hartnoll <47927778+Hartnoll@users.noreply.github.com>
2022-04-19 14:37:56 +0100

    UPDATED recorder

--- time-arena-game/Assets/Scripts/Crystal/CrystalRecorder.cs
+++ time-arena-game/Assets/Scripts/Crystal/CrystalRecorder.cs
@@ -7,12 +7,17 @@ public class CrystalRecorder : MonoBehaviour
 {
     
     private Transform shardtransform;
-    private StreamWriter file;
+    private static StreamWriter file;
+
+    void Awake()
+    {
+        file = new StreamWriter(gameObject.name + ".txt");
+    }
     
     // Update is called once per frame
     void Update()
     {
-        using StreamWriter file = new StreamWriter("shardPositions.txt", append: true);
+        //using StreamWriter file = new StreamWriter("shardPositions.txt");
         shardtransform = this.transform;
         string line = shardtransform.position.ToString() + " " + shardtransform.rotation.ToString();
         file.WriteLine(line);
