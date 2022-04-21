97569b4e28f7e343007a97a7c7bc202160870971
Hartnoll <henryh5451@gmail.com>
2022-04-21 18:45:53 +0100

    complete

--- time-arena-game/Assets/Scripts/Crystal/CrystalController.cs
+++ time-arena-game/Assets/Scripts/Crystal/CrystalController.cs
@@ -18,24 +18,7 @@ public class CrystalController : MonoBehaviour
     {
         //get the number of the frame that the animation should start at from the parent prefab. 
         _startFrame = transform.parent.gameObject.GetComponent<CrystalParent>().startFrame;
-        _positions = new List<(Vector3, Quaternion)>();
-        foreach (string line in System.IO.File.ReadLines(gameObject.name + ".txt"))
-        {   
-            string[] data = line.Split('|'); 
-            string[] localPositionString = data[0].Split(' ');
-            string[] rotationString = data[1].Split(' ');
-            if (localPositionString.Length != 3)
-            {
-                throw new System.FormatException("component count mismatch. Expected 3 components but got " + localPositionString.Length);
-            }
-            if (rotationString.Length != 4)
-            {
-                throw new System.FormatException("component count mismatch. Expected 3 components but got " + rotationString.Length);
-            }
-            Vector3 localPosition = new Vector3(float.Parse(localPositionString[0]), float.Parse(localPositionString[1]), float.Parse(localPositionString[2]));
-            Quaternion rotation = new Quaternion(float.Parse(rotationString[1]), float.Parse(rotationString[2]), float.Parse(rotationString[3]), float.Parse(rotationString[0])); 
-            _positions.Add((localPosition, rotation));
-        }  
+        _positions = ReadFile();         
         _startPosition = _positions[0].Item1;
         _startRotation = _positions[0].Item2;
         _endPosition = _positions[_positions.Count - 1].Item1;
@@ -67,14 +50,14 @@ public class CrystalController : MonoBehaviour
     {
         int frame = _timeLord.GetYourFrame();
         // If the current frame being rendered is before the start of the 
-        // animation, then set the local position to default.
+        // - animation, then set the local position to default.
         if (frame <= _startFrame)
         {
             transform.localPosition = _startPosition;
             transform.rotation = _startRotation;
         }
         // Else if your current frame is after the animation has ended, then 
-        // set local postion of shard to the final position of the animation. 
+        // - set local postion of shard to the final position of the animation. 
         else if (frame >= (_startFrame + _animationLength))
         {
             transform.localPosition = _endPosition;
@@ -82,8 +65,33 @@ public class CrystalController : MonoBehaviour
         }
         else
         {
-            transform.localPosition = _positions[frame].Item1;
-            transform.rotation = _positions[frame].Item2;
+            transform.localPosition = _positions[frame - _startFrame].Item1;
+            transform.rotation = _positions[frame - _startFrame].Item2;
         }
+    }
+
+    // ReadFile() parses the file ascociated with the shard and then returns a 
+    // - list of the positions and rotations of the shard at each frame of the animation.
+    List<(Vector3, Quaternion)> ReadFile()
+    {   
+        _positions = new List<(Vector3, Quaternion)>();
+        foreach (string line in System.IO.File.ReadLines(gameObject.name + ".txt"))
+        {   
+            string[] data = line.Split('|'); 
+            string[] localPositionString = data[0].Split(' ');
+            string[] rotationString = data[1].Split(' ');
+            if (localPositionString.Length != 3)
+            {
+                throw new System.FormatException("component count mismatch. Expected 3 components but got " + localPositionString.Length);
+            }
+            if (rotationString.Length != 4)
+            {
+                throw new System.FormatException("component count mismatch. Expected 3 components but got " + rotationString.Length);
+            }
+            Vector3 localPosition = new Vector3(float.Parse(localPositionString[0]), float.Parse(localPositionString[1]), float.Parse(localPositionString[2]));
+            Quaternion rotation = new Quaternion(float.Parse(rotationString[1]), float.Parse(rotationString[2]), float.Parse(rotationString[3]), float.Parse(rotationString[0])); 
+            _positions.Add((localPosition, rotation));
+        }
+        return _positions;
     }
 }
