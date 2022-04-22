1c34aafc6e00a14daf55141983c91f8d7e5ca348
Hartnoll <47927778+Hartnoll@users.noreply.github.com>
2022-04-22 18:16:50 +0100

    Added the breaking-crystals to the map.

--- time-arena-game/Assets/Scripts/Crystal/CrystalParent.cs
+++ time-arena-game/Assets/Scripts/Crystal/CrystalParent.cs
@@ -5,4 +5,35 @@ using UnityEngine;
 public class CrystalParent : MonoBehaviour
 {
     public int startFrame;
+    public bool boxRequired;
+    private TimeLord _timeLord;
+    BoxCollider _ObjectCollider;
+
+    void OnEnable()
+    {
+        GameController.newTimeLord += OnNewTimeLord;
+    }
+    
+    void OnDisable()
+    {
+        GameController.newTimeLord -= OnNewTimeLord;
+    }
+
+    void Start()
+    {
+        _timeLord = GameObject.FindWithTag("GameController").GetComponent<GameController>().GetTimeLord();
+        _ObjectCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnNewTimeLord(TimeLord time)
+    {
+        _timeLord = time;
+    } 
+
+    void Update()
+    {
+        int frame = _timeLord.GetYourFrame();
+        if (frame >= startFrame && _ObjectCollider.isTrigger == false) _ObjectCollider.isTrigger = true;
+        else if (frame < startFrame && _ObjectCollider.isTrigger == true) _ObjectCollider.isTrigger = false;
+    }
 }
