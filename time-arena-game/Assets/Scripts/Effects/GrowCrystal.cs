8f658dcaf72dcbd4278baa48db15176b2904d347
ZacW <rs19764@bristol.ac.uk>
2022-04-23 15:12:46 +0100

    changed growth effect

--- time-arena-game/Assets/Scripts/Effects/GrowCrystal.cs
+++ time-arena-game/Assets/Scripts/Effects/GrowCrystal.cs
@@ -13,11 +13,11 @@ public class GrowCrystal : MonoBehaviour
     [SerializeField] private float _growthTime = 90;
     [SerializeField] private BoxCollider _ObjectCollider;
     private float _endFrame;
-    private float _yPosScale = 1.7345f;
-    private float _ScaleBase = 30;
-    private float _yMax = 60;
-    private float _xzScale = 15;
+    private float _ScaleBase = 1;
+    private float _yMax = 45;
+    private float _xzScale = 45;
     private float _yPosStart;
+    private Vector3 _startPos;
 
 
     void Start()
@@ -28,8 +28,7 @@ public class GrowCrystal : MonoBehaviour
         }
         else _timeLord = FindObjectOfType<GameController>().GetComponent<GameController>().GetTimeLord();
        _endFrame = _startFrame + _growthTime;
-       Vector3 position = gameObject.transform.position;
-       _yPosStart = position[1];
+       _startPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -47,10 +46,11 @@ public class GrowCrystal : MonoBehaviour
                 //modifiy values by progress
                 float xzSChange = (gP*_xzScale) + _ScaleBase;
                 float ySChange = (gP*_yMax) + _ScaleBase;
-                float yPosChange = (gP* 1.4f);
+                float yPosChange = (gP* 2.15f);
                 Vector3 scaleChange = new Vector3(xzSChange,ySChange,xzSChange);
                 Vector3 position = gameObject.transform.position;
-                Vector3 positionChange = new Vector3(position[0], _yPosStart + yPosChange,position[2]);
+                Vector3 positionChange = _startPos;
+                positionChange[1] +=  yPosChange;
                 gameObject.transform.position = positionChange;
                 gameObject.transform.localScale = scaleChange;
             } 
