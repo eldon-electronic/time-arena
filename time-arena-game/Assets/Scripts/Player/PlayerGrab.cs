d1615e748e142653db2407c8a7848bf19871d12d
Angus Robertson <angusprobertson@gmail.com>
2022-04-24 21:52:41 +0100

    working grab function locally

--- time-arena-game/Assets/Scripts/Player/PlayerGrab.cs
+++ time-arena-game/Assets/Scripts/Player/PlayerGrab.cs
@@ -5,34 +5,33 @@ public class PlayerGrab : MonoBehaviour
 {
     [SerializeField] private Animator PlayerAnim;
 	[SerializeField] private LayerMask GrabMask;
-    [SerializeField] private PlayerController _player;
-	private float _grabCheckRadius = 1f;
+  [SerializeField] private PlayerController _player;
+  [SerializeField] private SphereCollider collider;
 	private bool _damageWindow = false;
 
     void Update()
     {
-       
+      // If grabbing, check for intersection with player.
+  		if (_damageWindow)
+  		{
+  			Collider[] playersGrab = Physics.OverlapSphere(collider.gameObject.transform.position, collider.radius, GrabMask);
+  			foreach (var playerGotGrab in playersGrab)
+  			{
+  				// Call grabplayer function on that player.
+  				PlayerController targetPlayer = playerGotGrab.GetComponent<PlayerController>();
+  				if (_player.Team == Constants.Team.Guardian &&
+  					targetPlayer.Team == Constants.Team.Miner)
+  				{
+  					// TODO: grab the miner.
+  					Debug.Log("Grabbed a miner");
+  				}
+  			}
+      }
     }
 
     public void Grab()
 	{
-		// If grabbing, check for intersection with player.
-		if (!_damageWindow)
-		{
-			Collider[] playersGrab = Physics.OverlapSphere(transform.position, _grabCheckRadius, GrabMask);
-			foreach (var playerGotGrab in playersGrab)
-			{
-				// Call grabplayer function on that player.
-				PlayerController targetPlayer = playerGotGrab.GetComponent<PlayerController>();
-				if (_player.Team == Constants.Team.Guardian && 
-					targetPlayer.Team == Constants.Team.Miner)
-				{
-					// TODO: grab the miner.
-					Debug.Log("Grabbed a miner");
-				}
-			}
-			
-		}
+      StartGrabbing();
 	}
 
     // Function called by animation to enable player to grab others.
@@ -40,7 +39,7 @@ public class PlayerGrab : MonoBehaviour
 	{
 		_damageWindow = true;
 		PlayerAnim.SetBool("isGrabbing", true);
-		
+
 	}
 
 	// Function called by animation to disable player to grab others.
