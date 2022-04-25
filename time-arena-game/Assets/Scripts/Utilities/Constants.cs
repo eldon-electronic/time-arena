aeced8dddf5c2798d41bee60665777fa30d73e1c
PaoloMura <paolo@muraonline.com>
2022-04-25 12:24:51 +0100

    changed frame rate and pregame length

--- time-arena-game/Assets/Scripts/Utilities/Constants.cs
+++ time-arena-game/Assets/Scripts/Utilities/Constants.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 public static class Constants
 {
     public static int GameLength = 5 * 60;
-    public static int PreGameLength = 120; // 3 * 60;
-    public static int FrameRate = 15; // 60;
+    public static int PreGameLength = 3 * 60;
+    public static int FrameRate = 30;
     public static int MaxPlayers = 10;
     public static int MinTimeSnapDistance = 10 * FrameRate;
     public static int TimeTravelVelocity = 10;
