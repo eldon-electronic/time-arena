7437398c2d4ba2fcb64a497e94fbc65eaca8db73
nisabayraktar <nisabayraktar.nb@gmail.com>
2022-05-05 01:18:14 +0100

    text added to tutorial2 events

--- time-arena-game/Assets/Scripts/Sprites/YourPlayer/Tutorial2.cs
+++ time-arena-game/Assets/Scripts/Sprites/YourPlayer/Tutorial2.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class Tutorial2: MonoBehaviour
 {
+    [SerializeField] private HudTutorial _tutorialHud;
+    [SerializeField] private PlayerController _player;
+
     void OnEnable()
     {
         TutorialCamera.collectableAppears += OnCollectableAppears;
@@ -27,7 +30,14 @@ public class Tutorial2: MonoBehaviour
 
     private void OnCollectableAppears()
     {
-
+        if(_player.Team == Constants.Team.Miner){
+            _tutorialHud.SetMessage("This the collectable crystal, you should run through it to collect.");
+        }
+        else{
+            _tutorialHud.SetMessage("This the Miner, catch them and steal their crystals!");
+        }
+        
+        
     }
 
     private void OnCollectableDisappears()
@@ -37,21 +47,34 @@ public class Tutorial2: MonoBehaviour
 
     private void OnObstacleGrows()
     {
+        _tutorialHud.SetMessage("These crystals are obstacles you should time travel to pass them.");
 
     }
 
     private void OnFaceYourself()
     {
-
+        if(_player.Team == Constants.Team.Miner){
+            _tutorialHud.SetMessage("This is you Miner.");
+        }
+        else{
+            _tutorialHud.SetMessage("This is you Guardian.");
+        }
     }
 
     private void OnCheckTracker()
     {
+         if(_player.Team == Constants.Team.Miner){
+            _tutorialHud.SetMessage("And this is your tracker which helps you to find the nearest crystal.");
+        }
+        else{
+            _tutorialHud.SetMessage("Let's start playing.");
+        }
 
     }
 
     private void OnGoodLuck()
     {
+        _tutorialHud.SetMessage("Good luck!");
 
     }
 
